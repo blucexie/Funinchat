57caf0aac6d874392e63be18a0818b0806efe6a7
blucexie <649453836@qq.com>
2017-03-07 17:45:57 +0800

    2017.03.07

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 /**
  * Created by funinbook on 2017/1/10.
  */
+
 $(function () {
     $('.close').click(function () {
         $a1.css('color','#000');
@@ -64,4 +65,111 @@ $(function () {
         }
     );
 
+   /* 娱信评分*/
+    function grade1(){
+    $('.line1').fadeIn(500, function () {
+        $('.t1').slideDown(500, function () {
+            $('.line2').fadeIn(500, function () {
+                $('.t2').slideDown(500, function () {
+                    $('.line3').fadeIn(500, function () {
+                        $('.t3').slideDown(500, function () {
+                            $('.line4').fadeIn(500, function () {
+                                $('.t4').slideDown(500, function () {
+                                    $('.line5').fadeIn(500, function () {
+                                        $('.t5').slideDown(500, function () {
+                                            $('.line6').fadeIn(500, function () {
+                                                $('.t6').slideDown(500)
+                                            });
+                                        });
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
+    }
+
+   $(window).scroll(function () {
+       var clientH = $(window).height();
+       var scrollH = $(window).scrollTop();
+       var scrollT = $('.spot').offset().top;
+       if(clientH+scrollH>=scrollT){
+           grade1()
+       }
+   });
+
+var num = 1;
+$('.next').click(function () {
+   if(num == 1){
+       $('.grade1').fadeOut(2000);
+       $('.grade2').fadeIn(2000);
+       num = 2;
+   }else if(num ==2){
+       $('.grade2').fadeOut(2000);
+       $('.grade3').fadeIn(2000);
+       num = 3;
+   }else if(num ==3){
+       $('.grade3').fadeOut(2000);
+       $('.grade1').fadeIn(2000);
+       num = 1;
+   }
+
+    if(a=3){
+        a=1
+    }
+
+});
+/*线条波浪*/
+    var waves = new SineWaves({
+        el: document.getElementById('waves'),
+
+        speed: 3,
+
+        width: function() {
+            return $(window).width();
+        },
+
+        height: function() {
+            return $(window).height();
+        },
+
+        ease: 'SineInOut',
+
+        wavesWidth: '100%',
+
+        waves: [
+            {
+                timeModifier: 4,
+                lineWidth: 2,
+                amplitude: -50,
+                wavelength: 85
+            },
+            {
+                timeModifier: 2,
+                lineWidth: 1,
+                amplitude: -80,
+                wavelength: 80
+            }
+        ],
+
+        // Resize
+        resizeEvent: function() {
+            var gradient = this.ctx.createLinearGradient(0, 0, this.width, 0);
+            gradient.addColorStop(0,"rgba(36,185,243,1)");
+
+            var index = -1;
+            var length = this.waves.length;
+            while(++index < length){
+                this.waves[index].strokeStyle = gradient;
+            }
+
+            // Clean Up
+            index = void 0;
+            length = void 0;
+            gradient = void 0;
+        }
+    });
 });
