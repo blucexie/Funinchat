2e8df05fe719b79b12c5eb7782432d7170ef92b8
谢玉琪 <649453836@qq.com>
2017-03-15 10:41:09 +0800

    2017.03.15

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@
  */
 
 $(function () {
+    
     $('.close').click(function () {
         $a1.css('color','#000');
         $a1.css('border-bottom','none');
@@ -91,11 +92,115 @@ $(function () {
         });
     });
     }
+/*人气Pk数字变化*/
+    var popNum =parseInt($('.fansL span em').html());
+    var popNum2 =parseInt($('.fansR span em').html());
+    function pop(){
+        popNum++;
+        $('.fansL span em').html(popNum);
+        if(popNum==250){
+            clearInterval(time);
+        }
+    }
+    function popJ(){
+        popNum--;
+        $('.fansL span em').html(popNum);
+        if(popNum==230){
+            clearInterval(timeJ);
+        }
+    }
+    function popR(){
+        popNum2++;
+        $('.fansR span em').html(popNum2);
+        if(popNum2==50){
+            clearInterval(time2);
+        }
+    }
+    function popRJ(){
+        popNum2--;
+        $('.fansR span em').html(popNum2);
+        if(popNum2==13){
+            clearInterval(time2J);
+        }
+    }
+
+    var time = null;
+    var timeJ = null;
+    var time2 =null;
+    var time2J = null;
+    $('.fansL').hover(function () {
+        clearInterval(timeJ);
+        time = setInterval(pop,100);
+    }, function () {
+        clearInterval(time);
+        timeJ = setInterval(popJ,100);
+    });
+    $('.fansR').hover(function () {
+        clearInterval(time2J);
+        time2 = setInterval(popR,100);
+    }, function () {
+        clearInterval(time2);
+        time2J = setInterval(popRJ,100);
+    });
+
+    /*步数Pk数字变化*/
+  var walk1 = parseInt($('.walk1 p span').html());
+  var walk2 = parseInt($('.walk2 p span').html());
+  var timeW1 = null;
+  var timeW1J = null;
+  var timeW2 = null;
+  var timeW2J = null;
+
+    function walk() {
+        walk1++;
+        $('.walk1 p span').html(walk1);
+        if(walk1==409000){
+            clearInterval(timeW1)
+        }
+    }
+    function walkJ() {
+        walk1--;
+        $('.walk1 p span').html(walk1);
+        if(walk1==408980){
+            clearInterval(timeW1J)
+        }
+    }
+    function walkR() {
+        walk2++;
+        $('.walk2 p span').html(walk2);
+        if(walk2==209000){
+            clearInterval(timeW2)
+        }
+    }
+    function walkRJ() {
+        walk2--;
+        $('.walk2 p span').html(walk2);
+        if(walk2==208980){
+            clearInterval(timeW2J)
+        }
+    }
+    $('.walk1').hover(function () {
+        clearInterval(timeW1J);
+        timeW1 = setInterval(walk,100);
+    }, function () {
+        clearInterval(timeW1);
+        timeW1J = setInterval(walkJ,100);
+    });
+
+    $('.walk2').hover(function () {
+        clearInterval(timeW2J);
+        timeW2 = setInterval(walkR,100);
+    }, function () {
+        clearInterval(timeW2);
+        timeW2J = setInterval(walkRJ,100);
+    });
 
    $(window).scroll(function () {
        var clientH = $(window).height();
        var scrollH = $(window).scrollTop();
        var scrollT = $('.spot').offset().top;
+
+       var scrollPopT = $('.fansR').offset().top;
        if(clientH+scrollH>=scrollT){
            grade1()
        }
@@ -106,14 +211,17 @@ $('.next').click(function () {
    if(num == 1){
        $('.grade1').fadeOut(2000);
        $('.grade2').fadeIn(2000);
+       $('#grade h2').html('娱信评分');
        num = 2;
    }else if(num ==2){
        $('.grade2').fadeOut(2000);
        $('.grade3').fadeIn(2000);
+       $('#grade h2').html('娱信画像');
        num = 3;
    }else if(num ==3){
        $('.grade3').fadeOut(2000);
        $('.grade1').fadeIn(2000);
+       $('#grade h2').html('娱信雷达图');
        num = 1;
    }
 
@@ -143,15 +251,15 @@ $('.next').click(function () {
         waves: [
             {
                 timeModifier: 4,
-                lineWidth: 2,
-                amplitude: -50,
-                wavelength: 85
+                lineWidth: 5,
+                amplitude: -300,
+                wavelength: 45
             },
             {
                 timeModifier: 2,
-                lineWidth: 1,
-                amplitude: -80,
-                wavelength: 80
+                lineWidth: 3,
+                amplitude: -200,
+                wavelength: 40
             }
         ],
 
@@ -172,4 +280,8 @@ $('.next').click(function () {
             gradient = void 0;
         }
     });
+
+
+
+
 });
